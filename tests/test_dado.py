610f148f7db39a2f8d41c78e5819e2056ba7ddd0
sshiro0 <francnunez2023@inf.udec.cl>
2025-08-28 16:31:09 -0400

    REFACTOR: mejora código de ver denominación del dado.

--- tests/test_dado.py
+++ tests/test_dado.py
@@ -17,27 +17,19 @@ class TestDado:
         mock_randint = MagicMock(side_effect=[1, 2, 3, 4, 5, 6])
         mocker.patch('juego.dado.random.randint', mock_randint)
 
-        dado1 = Dado()
-        dado2 = Dado()
-        dado3 = Dado()
-        dado4 = Dado()
-        dado5 = Dado()
-        dado6 = Dado()
+        dados = []
+        for i in range(6):
+            dado = Dado()
+            dados.append(dado)
 
-        assert dado1.get_valor() == Denominacion.AS.value
-        assert dado2.get_valor() == Denominacion.TONTO.value
-        assert dado3.get_valor() == Denominacion.TREN.value
-        assert dado4.get_valor() == Denominacion.CUADRA.value
-        assert dado5.get_valor() == Denominacion.QUINA.value
-        assert dado6.get_valor() == Denominacion.SEXTO.value
+        denominaciones = [
+            Denominacion.AS, Denominacion.TONTO, Denominacion.TREN,
+            Denominacion.CUADRA, Denominacion.QUINA, Denominacion.SEXTO
+        ]
 
-        assert dado1.get_denominacion() == Denominacion.AS.name
-        assert dado2.get_denominacion() == Denominacion.TONTO.name
-        assert dado3.get_denominacion() == Denominacion.TREN.name
-        assert dado4.get_denominacion() == Denominacion.CUADRA.name
-        assert dado5.get_denominacion() == Denominacion.QUINA.name
-        assert dado6.get_denominacion() == Denominacion.SEXTO.name
+        for i in range(6):
+            assert dados[i].get_valor() == denominaciones[i].value
+            assert dados[i].get_denominacion() == denominaciones[i].name
 
         assert mock_randint.call_count == 6
         mock_randint.assert_called_with(1, 6)
-
