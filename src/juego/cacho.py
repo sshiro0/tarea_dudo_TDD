505e3665a3fa85b3192a2bdd19e771c0c9de1f17
sshiro0 <francnunez2023@inf.udec.cl>
2025-08-28 17:41:39 -0400

    GREEN: implementación mínima para crear cacho.

---
+++ src/juego/cacho.py
@@ -0,0 +1,15 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from juego.dado import Dado
+
+class Cacho:
+    def __init__(self):
+        self.dados = []
+        for i in range(5):
+            self.dados.append(Dado())
+
+    def get_lista_dados(self):
+        return self.dados
+
