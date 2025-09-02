4af6f335d904945f1d5bc477baaa6b31aeba47cf
unknown <matgarcia2023@inf.udec.cl>
2025-09-01 22:35:50 -0400

    GREEN: implementación mínima para definir al jugador inicial.

--- src/juego/gestor_partida.py
+++ src/juego/gestor_partida.py
@@ -1,5 +1,8 @@
 import sys
 import os
+import random
+
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 from .cacho import Cacho
@@ -8,6 +11,12 @@ class GestorPartida:
     def __init__(self, cantidad):
         self.cantidad = cantidad
         self.jugadores = []
+        self.jugador_actual = None
 
         for i in range(cantidad):
             self.jugadores.append(Cacho())
+
+    def definir_jugador_inicial(self):
+        index = random.randint(0, self.cantidad - 1)
+        self.jugador_actual = self.jugadores[index]
+        return self.jugador_actual
